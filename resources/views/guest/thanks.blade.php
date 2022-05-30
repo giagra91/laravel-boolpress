f790952bab653199db2151bf7571d0094d48a1a6
giagra91 <giagra91@gmail.com>
2022-05-30 17:23:21 +0200

    add session's message for sended email

--- resources/views/guest/thanks.blade.php
+++ resources/views/guest/thanks.blade.php
@@ -4,7 +4,12 @@
 @section('content')
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12">
+            @if (session("message"))
+                <div class=" col-6 mx-auto text-center alert alert-success">
+                    {{ session("message") }}
+                </div>
+            @endif
+            <div class="col-12 text-center">
                 <h1>Grazie per averci contattato, a breve avrai le informazioni richieste.</h1>
             </div>
         </div>
